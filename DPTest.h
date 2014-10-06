d651c63fa2aa3635e348b695d039807137d7d18f
David Park <davidpark@Davids-MacBook-Pro-4.local>
2014-10-05 21:32:04 -0500

    added a test class

---
+++ DPTest.h
@@ -0,0 +1,13 @@
+//
+//  DPTest.h
+//  Git Test
+//
+//  Created by David Park on 10/5/14.
+//  Copyright (c) 2014 DP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DPTest : NSObject
+
+@end
---
+++ DPTest.m
@@ -0,0 +1,13 @@
+//
+//  DPTest.m
+//  Git Test
+//
+//  Created by David Park on 10/5/14.
+//  Copyright (c) 2014 DP. All rights reserved.
+//
+
+#import "DPTest.h"
+
+@implementation DPTest
+
+@end
